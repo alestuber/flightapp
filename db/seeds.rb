92cf91fb4692ee63efebb66137d2e66d0dc9c5ee
Alexandre Stuber <alestuber@gmail.com>
2015-07-25 17:59:19 -0300

    Create airport model and seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+%w(SFO NYC SPO WHA LON PAR RIO TKO MIL ROM FRA).each do |airport|
+  Airport.create code: airport
+end
+
