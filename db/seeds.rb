2cdc71c4fda5caa3bae6f424360d0c6b5cffa7f8
Alexandre Stuber <alestuber@gmail.com>
2015-07-25 19:07:02 -0300

    Add rest of seed data to flights

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,8 +10,8 @@
   Airport.create code: airport
 end
 
-50.times do
+150.times do
   rand_travel = (1..(Airport.count) ).sort_by{rand}[1..2]
-  Flight.create(start_id: Airport.find(rand_travel[0]).id, end_id: Airport.find(rand_travel[1]).id)
+  Flight.create(start_id: Airport.find(rand_travel[0]).id, end_id: Airport.find(rand_travel[1]).id, start_time: rand(Time.now..(Time.now + 30).to_time), duration: rand(60..(20*60)))
 end
 
