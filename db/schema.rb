d1313fa6bec79e110d5fb575ec74cf7e65874ec6
Alexandre Stuber <alestuber@gmail.com>
2015-07-26 15:11:38 -0300

    Generate Passenger model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150725210224) do
+ActiveRecord::Schema.define(version: 20150726181119) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,5 +33,12 @@ ActiveRecord::Schema.define(version: 20150725210224) do
 
   add_index "flights", ["end_id"], name: "index_flights_on_end_id", using: :btree
   add_index "flights", ["start_id"], name: "index_flights_on_start_id", using: :btree
+
+  create_table "passengers", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
